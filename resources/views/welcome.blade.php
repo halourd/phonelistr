dbdd7843f2257ad22ab09e02bdd221c86459fc67
haro-at-qx <161855853+haro-at-qx@users.noreply.github.com>
2024-06-22 20:57:19 +0800

    Change to Phonebook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Phonebook</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        <style>
-
-        </style>
         @vite('resources/css/app.css')
         @vite(['resources/js/app.js'])
     </head>
